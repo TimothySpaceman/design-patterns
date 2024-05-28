e45af28acd855902bb2f896a0a37dd5f7d0a9d71
Tymofiy Regenel <timothy.rehenel@gmail.com>
2024-05-29 00:06:25 +0300

    breadth first and depth first iterators

---
+++ lab-5/Iterators/BreadthIterator.cs
@@ -0,0 +1,55 @@
+﻿using lab_5.Nodes;
+
+namespace lab_5.Iterators;
+
+public class BreadthIterator: INodeIterator
+{
+    public List<LightNode> Nodes = new List<LightNode>();
+    public Queue<LightNode> Queue = new Queue<LightNode>();
+    public int CurrentIndex = 0;
+
+    public BreadthIterator(LightNode root)
+    {
+        Queue.Enqueue(root);
+        AddChildNodes();
+    }
+
+    private void AddChildNodes()
+    {
+        var CurrentQueue = new Queue<LightNode>();
+        while(Queue.Any())
+        { 
+            CurrentQueue.Enqueue(Queue.Dequeue());
+        }
+        while(CurrentQueue.Any())
+        {
+            var node = CurrentQueue.Dequeue();
+            Nodes.Add(node);
+            if (node is not LightElementNode elementNode) continue;
+            foreach (var child in elementNode.Children)
+            {
+                Queue.Enqueue(child);
+            }
+        }
+
+        if (Queue.Any())
+        {
+            AddChildNodes();
+        }
+    }
+
+    public bool HasMore()
+    {
+        return CurrentIndex < Nodes.Count - 1;
+    }
+
+    public LightNode? Next()
+    {
+        if (!HasMore())
+        {
+            return null;
+        }
+
+        return Nodes[CurrentIndex++];
+    }
+}
--- lab-5/Program.cs
+++ lab-5/Program.cs
@@ -1,12 +1,27 @@
-﻿using lab_5.Nodes;
+﻿using lab_5.Iterators;
+using lab_5.Nodes;
 
 var body = new Body(new List<string>{"border-double"}, new List<LightNode>
 {
     new Div(new List<string>{"bg-red", "bg-red"}, new List<LightNode>
     {
-        new P(null, "Welcome to LightHTML!"),
-        new P(new List<string>{"fg-darkred"}, "Have fun!")
+        new P(null, "1"),
+        new P(new List<string>{"fg-darkred"}, "2")
+    }),
+    new Div(new List<string>{"bg-red", "bg-red"}, new List<LightNode>
+    {
+        new Div(new List<string>{"bg-red", "bg-red"}, new List<LightNode>
+        {
+            new P(null, "3"),
+            new P(new List<string>{"fg-darkred"}, "4")
+        }),
+        new P(new List<string>{"fg-darkred"}, "5")
     })
 });
 
-Console.WriteLine(body.OuterHTML());
+var iterator = new DepthIterator(body);
+
+Console.WriteLine($"Root: {iterator.Next()?.OuterHTML()}");
+Console.WriteLine($"Next: {iterator.Next()?.OuterHTML()}");
+Console.WriteLine($"Next: {iterator.Next()?.OuterHTML()}");
+Console.WriteLine($"Next: {iterator.Next()?.OuterHTML()}");
